c7ae31e3933656f6b38501293a341079ae292aa9
wcaokaze <wcaokaze@gmail.com>
2020-01-21 00:52:00 +0900

    added GetterTest

---
+++ vue-android-core/src/main/java/vue/GetterField.kt
@@ -0,0 +1,3 @@
+package vue
+
+class GetterField<out T>
---
+++ vue-android-core/src/test/java/vue/GetterTest.kt
@@ -0,0 +1,9 @@
+package vue
+
+import kotlin.test.*
+import org.junit.runner.*
+import org.junit.runners.*
+
+@RunWith(JUnit4::class)
+class GetterTest {
+}
