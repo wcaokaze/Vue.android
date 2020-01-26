5f7d29d45f8f3a3739ed6a0b84e76234151b2947
wcaokaze <wcaokaze@gmail.com>
2020-01-27 01:58:10 +0900

    refactored: made the Activity for test empty

--- vue-android-core/src/androidTest/java/vue/VBindTest.kt
+++ vue-android-core/src/androidTest/java/vue/VBindTest.kt
@@ -8,31 +8,35 @@ import kotlin.test.*
 import kotlin.test.Test
 
 import android.app.*
-import android.os.*
 import android.view.*
 
-class VBindTestActivity : Activity() {
-   lateinit var view: View private set
-
-   override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
-      view = View(this)
-   }
-}
+class VBindTestActivity : Activity()
 
 @RunWith(AndroidJUnit4::class)
 class VBindTest {
    @get:Rule
    val activityScenarioRule = activityScenarioRule<VBindTestActivity>()
 
-   @Test
-   fun bind_initialValue() {
+   @Test fun bind() {
       activityScenarioRule.scenario.onActivity { activity ->
-         val view = activity.view
          val state = StateField(false)
+         val view = View(activity)
+         view.vBind.isVisible(state)
+         activity.setContentView(view)
+
+         assertEquals(1, state.observerCount)
+      }
+   }
+
+   @Test fun bind_initialValue() {
+      activityScenarioRule.scenario.onActivity { activity ->
+         val state = StateField(false)
+         val view = View(activity)
 
          view.visibility = View.VISIBLE
          view.vBind.isVisible(state)
+         activity.setContentView(view)
+
          assertEquals(View.INVISIBLE, view.visibility)
       }
    }
