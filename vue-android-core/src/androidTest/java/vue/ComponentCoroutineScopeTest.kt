fe706ee2ed9f09da089ed4d38721eb61d5029b33
wcaokaze <wcaokaze@gmail.com>
2020-02-04 01:55:11 +0900

    implemented ComponentScope

---
+++ vue-android-core/src/androidTest/java/vue/ComponentCoroutineScopeTest.kt
@@ -0,0 +1,48 @@
+package vue
+
+import androidx.test.ext.junit.rules.*
+import androidx.test.ext.junit.runners.*
+import org.junit.*
+import org.junit.runner.*
+import kotlin.test.*
+import kotlin.test.Test
+
+import android.content.*
+import android.view.*
+import kotlinx.coroutines.*
+
+class CoroutineScopeTestComponent(context: Context) : VComponent, CoroutineScope {
+   override val view = View(context)
+   override val coroutineContext by ComponentScope()
+}
+
+@RunWith(AndroidJUnit4::class)
+class ComponentCoroutineScopeTest {
+   @get:Rule
+   val activityScenarioRule = activityScenarioRule<EmptyTestActivity>()
+
+   @Test fun inactiveUntilMounted() {
+      lateinit var component: CoroutineScopeTestComponent
+
+      activityScenarioRule.scenario
+            .onActivity { activity ->
+               component = CoroutineScopeTestComponent(activity)
+            }
+            .onActivity {
+               assertFalse(component.isActive)
+            }
+   }
+
+   @Test fun activeWhileMounted() {
+      lateinit var component: CoroutineScopeTestComponent
+
+      activityScenarioRule.scenario
+            .onActivity { activity ->
+               component = CoroutineScopeTestComponent(activity)
+               activity.setContentView(component.view)
+            }
+            .onActivity {
+               assertTrue(component.isActive)
+            }
+   }
+}
