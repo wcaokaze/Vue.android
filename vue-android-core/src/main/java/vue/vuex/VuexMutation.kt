9284edddf50aea5a3b90d5c1dc212beb723cdf92
wcaokaze <wcaokaze@gmail.com>
2020-02-18 03:14:50 +0900

    setting State into Mutation

--- vue-android-core/src/main/java/vue/vuex/VuexMutation.kt
+++ vue-android-core/src/main/java/vue/vuex/VuexMutation.kt
@@ -3,6 +3,23 @@ package vue.vuex
 import androidx.annotation.*
 
 abstract class VuexMutation<S> where S : VuexState {
+   val state: S
+
+   init {
+      val storeStack = storeStack.get()
+
+      if (storeStack.isNullOrEmpty()) {
+         throw IllegalStateException(
+               "No VuexStore is ready. " +
+               "Maybe you attempt to instantiate VuexMutation without VuexStore?")
+      }
+
+      @Suppress("UNCHECKED_CAST")
+      val store = storeStack.last as VuexStore<S, *, *, *>
+
+      state = store.state
+   }
+
    var <T> VuexState.StateField<T>.value: T
       get() = value
       @UiThread set(v) {
--- vue-android-core/src/test/java/vue/vuex/StoreTest.kt
+++ vue-android-core/src/test/java/vue/vuex/StoreTest.kt
@@ -30,4 +30,33 @@ class StoreTest {
       assertSame(action,   store.action)
       assertSame(getter,   store.getter)
    }
+
+   @Test fun gettingStateFromMutation() {
+      class TestState    : VuexState()
+      class TestMutation : VuexMutation<TestState>()
+      class TestGetter   : VuexGetter<TestState>()
+      class TestAction   : VuexAction<TestState, TestMutation, TestGetter>()
+
+      class TestStore : VuexStore<TestState, TestMutation, TestAction, TestGetter>() {
+         override fun createState()    = TestState()
+         override fun createMutation() = TestMutation()
+         override fun createAction()   = TestAction()
+         override fun createGetter()   = TestGetter()
+      }
+
+      val store = TestStore()
+      val state    = store.state
+      val mutation = store.mutation
+
+      assertSame(state, mutation.state)
+   }
+
+   @Test fun mutationCannotInstantiateWithoutStore() {
+      class TestState : VuexState()
+      class TestMutation : VuexMutation<TestState>()
+
+      assertFailsWith<IllegalStateException> {
+         TestMutation()
+      }
+   }
 }
