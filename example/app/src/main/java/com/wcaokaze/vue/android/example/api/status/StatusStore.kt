56bfa02f3c305efd78096756f22f4872b9f4bc1e
wcaokaze <wcaokaze@gmail.com>
2020-04-27 03:02:45 +0900

    added entities and Stores

---
+++ example/app/src/main/java/com/wcaokaze/vue/android/example/api/status/StatusStore.kt
@@ -0,0 +1,25 @@
+package com.wcaokaze.vue.android.example.api.status
+
+import vue.*
+import vue.vuex.*
+
+class StatusStore : VuexStore<StatusState, StatusMutation, StatusAction, StatusGetter>() {
+   override fun createState()    = StatusState()
+   override fun createMutation() = StatusMutation()
+   override fun createAction()   = StatusAction()
+   override fun createGetter()   = StatusGetter()
+}
+
+class StatusState : VuexState() {
+   val statuses = state<Map<Status.Id, Status>>(emptyMap())
+}
+
+class StatusMutation : VuexMutation<StatusState>()
+
+class StatusAction : VuexAction<StatusState, StatusMutation, StatusGetter>()
+
+class StatusGetter : VuexGetter<StatusState>() {
+   fun getStatus(id: Status.Id): ReactiveField<Status?> = getter {
+      state.statuses()[id]
+   }
+}
---
+++ example/app/src/main/java/com/wcaokaze/vue/android/example/api/timeline/TimelineStore.kt
@@ -0,0 +1,21 @@
+package com.wcaokaze.vue.android.example.api.timeline
+
+import com.wcaokaze.vue.android.example.api.status.*
+import vue.vuex.*
+
+class TimelineStore : VuexStore<TimelineState, TimelineMutation, TimelineAction, TimelineGetter>() {
+   override fun createState()    = TimelineState()
+   override fun createMutation() = TimelineMutation()
+   override fun createAction()   = TimelineAction()
+   override fun createGetter()   = TimelineGetter()
+}
+
+class TimelineState : VuexState() {
+   val statusIds = state<List<Status.Id>>(emptyList())
+}
+
+class TimelineMutation : VuexMutation<TimelineState>()
+
+class TimelineAction : VuexAction<TimelineState, TimelineMutation, TimelineGetter>()
+
+class TimelineGetter : VuexGetter<TimelineState>()
