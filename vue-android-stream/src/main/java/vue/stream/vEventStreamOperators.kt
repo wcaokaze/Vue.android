f478386febd1e5c8913db729919ca0d3bd347f7c
wcaokaze <wcaokaze@gmail.com>
2020-05-11 10:45:15 +0900

    added VEvent2..8.map

--- vue-android-stream/src/main/java/vue/stream/vEventStreamOperators.kt
+++ vue-android-stream/src/main/java/vue/stream/vEventStreamOperators.kt
@@ -31,3 +31,94 @@ inline fun <A, R> VEvent1<A>.map(
       }
    }
 }
+
+inline fun <A, B, R> VEvent2<A, B>.map(
+      crossinline mapper: suspend (A, B) -> R
+): VEvent1<R> {
+   return object : VEvent1<R> {
+      override fun invoke(coroutineContext: CoroutineContext, action: suspend (R) -> Unit) {
+         this@map.invoke(coroutineContext) { a, b ->
+            val mapped = mapper(a, b)
+            action(mapped)
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, R> VEvent3<A, B, C>.map(
+      crossinline mapper: suspend (A, B, C) -> R
+): VEvent1<R> {
+   return object : VEvent1<R> {
+      override fun invoke(coroutineContext: CoroutineContext, action: suspend (R) -> Unit) {
+         this@map.invoke(coroutineContext) { a, b, c ->
+            val mapped = mapper(a, b, c)
+            action(mapped)
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, R> VEvent4<A, B, C, D>.map(
+      crossinline mapper: suspend (A, B, C, D) -> R
+): VEvent1<R> {
+   return object : VEvent1<R> {
+      override fun invoke(coroutineContext: CoroutineContext, action: suspend (R) -> Unit) {
+         this@map.invoke(coroutineContext) { a, b, c, d ->
+            val mapped = mapper(a, b, c, d)
+            action(mapped)
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, E, R> VEvent5<A, B, C, D, E>.map(
+      crossinline mapper: suspend (A, B, C, D, E) -> R
+): VEvent1<R> {
+   return object : VEvent1<R> {
+      override fun invoke(coroutineContext: CoroutineContext, action: suspend (R) -> Unit) {
+         this@map.invoke(coroutineContext) { a, b, c, d, e ->
+            val mapped = mapper(a, b, c, d, e)
+            action(mapped)
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, E, F, R> VEvent6<A, B, C, D, E, F>.map(
+      crossinline mapper: suspend (A, B, C, D, E, F) -> R
+): VEvent1<R> {
+   return object : VEvent1<R> {
+      override fun invoke(coroutineContext: CoroutineContext, action: suspend (R) -> Unit) {
+         this@map.invoke(coroutineContext) { a, b, c, d, e, f ->
+            val mapped = mapper(a, b, c, d, e, f)
+            action(mapped)
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, E, F, G, R> VEvent7<A, B, C, D, E, F, G>.map(
+      crossinline mapper: suspend (A, B, C, D, E, F, G) -> R
+): VEvent1<R> {
+   return object : VEvent1<R> {
+      override fun invoke(coroutineContext: CoroutineContext, action: suspend (R) -> Unit) {
+         this@map.invoke(coroutineContext) { a, b, c, d, e, f, g ->
+            val mapped = mapper(a, b, c, d, e, f, g)
+            action(mapped)
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, E, F, G, H, R> VEvent8<A, B, C, D, E, F, G, H>.map(
+      crossinline mapper: suspend (A, B, C, D, E, F, G, H) -> R
+): VEvent1<R> {
+   return object : VEvent1<R> {
+      override fun invoke(coroutineContext: CoroutineContext, action: suspend (R) -> Unit) {
+         this@map.invoke(coroutineContext) { a, b, c, d, e, f, g, h ->
+            val mapped = mapper(a, b, c, d, e, f, g, h)
+            action(mapped)
+         }
+      }
+   }
+}
