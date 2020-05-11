8b99146f6481257386680b807e378fc4d6493123
wcaokaze <wcaokaze@gmail.com>
2020-05-11 11:20:53 +0900

    added VEvent2..8.filter

--- vue-android-stream/src/main/java/vue/stream/vEventFilter.kt
+++ vue-android-stream/src/main/java/vue/stream/vEventFilter.kt
@@ -35,3 +35,122 @@ inline fun <A> VEvent1<A>.filter(
       }
    }
 }
+
+inline fun <A, B> VEvent2<A, B>.filter(
+      crossinline filter: suspend (A, B) -> Boolean
+): VEvent2<A, B> {
+   return object : VEvent2<A, B> {
+      override fun invoke(
+            coroutineContext: CoroutineContext,
+            action: suspend (A, B) -> Unit
+      ) {
+         this@filter.invoke(coroutineContext) { a, b ->
+            if (filter(a, b)) {
+               action(a, b)
+            }
+         }
+      }
+   }
+}
+
+inline fun <A, B, C> VEvent3<A, B, C>.filter(
+      crossinline filter: suspend (A, B, C) -> Boolean
+): VEvent3<A, B, C> {
+   return object : VEvent3<A, B, C> {
+      override fun invoke(
+            coroutineContext: CoroutineContext,
+            action: suspend (A, B, C) -> Unit
+      ) {
+         this@filter.invoke(coroutineContext) { a, b, c ->
+            if (filter(a, b, c)) {
+               action(a, b, c)
+            }
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D> VEvent4<A, B, C, D>.filter(
+      crossinline filter: suspend (A, B, C, D) -> Boolean
+): VEvent4<A, B, C, D> {
+   return object : VEvent4<A, B, C, D> {
+      override fun invoke(
+            coroutineContext: CoroutineContext,
+            action: suspend (A, B, C, D) -> Unit
+      ) {
+         this@filter.invoke(coroutineContext) { a, b, c, d ->
+            if (filter(a, b, c, d)) {
+               action(a, b, c, d)
+            }
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, E> VEvent5<A, B, C, D, E>.filter(
+      crossinline filter: suspend (A, B, C, D, E) -> Boolean
+): VEvent5<A, B, C, D, E> {
+   return object : VEvent5<A, B, C, D, E> {
+      override fun invoke(
+            coroutineContext: CoroutineContext,
+            action: suspend (A, B, C, D, E) -> Unit
+      ) {
+         this@filter.invoke(coroutineContext) { a, b, c, d, e ->
+            if (filter(a, b, c, d, e)) {
+               action(a, b, c, d, e)
+            }
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, E, F> VEvent6<A, B, C, D, E, F>.filter(
+      crossinline filter: suspend (A, B, C, D, E, F) -> Boolean
+): VEvent6<A, B, C, D, E, F> {
+   return object : VEvent6<A, B, C, D, E, F> {
+      override fun invoke(
+            coroutineContext: CoroutineContext,
+            action: suspend (A, B, C, D, E, F) -> Unit
+      ) {
+         this@filter.invoke(coroutineContext) { a, b, c, d, e, f ->
+            if (filter(a, b, c, d, e, f)) {
+               action(a, b, c, d, e, f)
+            }
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, E, F, G> VEvent7<A, B, C, D, E, F, G>.filter(
+      crossinline filter: suspend (A, B, C, D, E, F, G) -> Boolean
+): VEvent7<A, B, C, D, E, F, G> {
+   return object : VEvent7<A, B, C, D, E, F, G> {
+      override fun invoke(
+            coroutineContext: CoroutineContext,
+            action: suspend (A, B, C, D, E, F, G) -> Unit
+      ) {
+         this@filter.invoke(coroutineContext) { a, b, c, d, e, f, g ->
+            if (filter(a, b, c, d, e, f, g)) {
+               action(a, b, c, d, e, f, g)
+            }
+         }
+      }
+   }
+}
+
+inline fun <A, B, C, D, E, F, G, H> VEvent8<A, B, C, D, E, F, G, H>.filter(
+      crossinline filter: suspend (A, B, C, D, E, F, G, H) -> Boolean
+): VEvent8<A, B, C, D, E, F, G, H> {
+   return object : VEvent8<A, B, C, D, E, F, G, H> {
+      override fun invoke(
+            coroutineContext: CoroutineContext,
+            action: suspend (A, B, C, D, E, F, G, H) -> Unit
+      ) {
+         this@filter.invoke(coroutineContext) { a, b, c, d, e, f, g, h ->
+            if (filter(a, b, c, d, e, f, g, h)) {
+               action(a, b, c, d, e, f, g, h)
+            }
+         }
+      }
+   }
+}
